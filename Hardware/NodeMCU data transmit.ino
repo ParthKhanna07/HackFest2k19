613b6a07a1cc4f97cb63185f9ad9a00991821bd5
TheLethargicOwl <thelethargicowl@gmail.co>
2019-03-16 09:43:39 +0530

    Hardware Backend

---
+++ Hardware/NodeMCU data transmit.ino
@@ -0,0 +1,61 @@
+
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "pk";  //ENTER YOUR WIFI SETTINGS
+const char *password = "parth@khanna";
+
+//Web/Server address to read/write from 
+const char *host = "https://driller.000webhostapp.com";  
+
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+  delay(1000);
+  WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  Serial.print("Connecting");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+}
+
+void loop() {
+  HTTPClient http;    //Declare object of class HTTPClient
+
+  String getData, Link;
+  //GET Data
+  getData = "?otp=0987&volume=1.23&mass=30" ;  //Note "?" added at front
+  Link = "http://driller.000webhostapp.com/client/functions/addTempData.php" + getData;
+  
+  http.begin(Link);     //Specify request destination
+  
+  int httpCode = http.GET();            //Send the request
+  String payload = http.getString();    //Get the response payload
+
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(payload);    //Print request response payload
+
+  http.end();  //Close connection
+  
+  delay(5000);   //GET Data at every 5 seconds
+}
+//=======================================================================
